615d6c878870b207aa0246c2442cee3c5073dd6f
Andrew <andrew_barnes99@hotmail.com>
2021-01-10 00:48:12 +1100

    update app.js

--- app.js
+++ app.js
@@ -25,11 +25,9 @@ mongoose.connect(connectionStr, {
 
 const app = express();
 
-const port = 3000
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 // view engine setup
 app.engine('ejs', require('express-ejs-extend'));
